d583e148756e1839ae8d4a36aaf35babba6f66b4
Taystan <52808097+tam-114@users.noreply.github.com>
2024-11-30 15:23:17 -0500

    update hero msg

--- pages/home.php
+++ pages/home.php
@@ -6,7 +6,7 @@
                 <svg class="flipping-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
                 </svg>
                 <h1 class="h1-text" >Freelance Web Designer</h1>
-                <p class="p-text py-lg-4">Creating websites for businesses and individuals that are accessible, inclusive, and diverse.</p>
+                <p class="p-text py-lg-4">Creating websites affordable solutions for growing businesses and impactful organizations.</p>
 
                 <div class="flex-row">
                     <a href="#explore" class="button-primary">Explore site</a>
