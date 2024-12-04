8187ce34c3697b60e3628bbfdafee8033c9a16c5
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-04 18:48:58 -0500

    scroll

--- pages/home.php
+++ pages/home.php
@@ -183,6 +183,11 @@
         <h2>Experience with these platforms</h2>
         <div class="custom-carousel-container">
             <div class="custom-carousel">
+            <img src="/assets/images/WordPress-logotype-standard 1 1.png" alt="Image 1" class="carousel-image">
+                <img src="/assets/images/Webflow_logo.svg" alt="Image 2" class="carousel-image">
+                <img src="/assets/images/shopify 1.png" alt="Image 3" class="carousel-image">
+                <img src="/assets/images/squarespace-logo-horizontal-black 1 1.png" alt="Image 4" class="carousel-image">
+                <img src="/assets/images/framer-logo 1.png" alt="Image 5" class="carousel-image">
                 <img src="/assets/images/WordPress-logotype-standard 1 1.png" alt="Image 1" class="carousel-image">
                 <img src="/assets/images/Webflow_logo.svg" alt="Image 2" class="carousel-image">
                 <img src="/assets/images/shopify 1.png" alt="Image 3" class="carousel-image">
