5cc5a2e2acc21788503ebd2f0ac557726eb283f6
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-04 18:56:50 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -62,7 +62,7 @@
 <!-- Scripts -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
  
-<script src="<?php echo BASE_URL; ?>assets/script.js"></script>
+<script src="/assets/script.js"></script>
 
 <!-- AOS -->
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
