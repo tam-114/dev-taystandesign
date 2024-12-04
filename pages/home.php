407acf034d6d0db4d426ef8873653105d5405197
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-04 17:58:05 -0500

    adding cms logos

--- pages/home.php
+++ pages/home.php
@@ -176,7 +176,23 @@
             <span class="full-width-line"></span>
         </div>
     </div>
- </section>
+</section>
+
+<section>
+    <div class="container">
+        <div class="carousel-container">
+            <div class="carousel">
+                <img src="/assets/images/WordPress-logotype-standard 1 1.png" alt="Image 1">
+                <img src="/assets/images/Webflow_logo.svg" alt="Image 2">
+                <img src="/assets/images/shopify 1.png" alt="Image 3">
+                <img src="/assets/images/squarespace-logo-horizontal-black 1 1.png" alt="Image 4">
+                <img src="/assets/images/framer-logo 1.png" alt="Image 5">
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 <section class="section-padding">
     <div class="container testimonials pt-4 mt-4">
