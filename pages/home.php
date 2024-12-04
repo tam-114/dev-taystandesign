7690b5ea8bd0e06b1fc6c5211a407961f5990923
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-03 23:22:59 -0500

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -516,8 +516,8 @@ $checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" vie
                     <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#5f6368"><path d="M0 0h24v24H0z" fill="none"/><path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/></svg>
                 </div>
                 <div class="faq_intro" >
-                    <h1 class="h1-faq">Here are some frequently asked questions</h1>
-                    <p class="p-text py-2">Don’t see an answer here that you’re looking for? Feel free to reach out to learn more!</p>
+                    <h1 class="h1-faq">Here are some frequently asked <span class="highlight">questions</span> </h1>
+                    <p class="p-text py-2">Don't see an answer here that you're looking for? Feel free to reach out to learn more!</p>
                     <a href="/contact" class="btn btn-secondary p-0">Reach out to me <i class="fa-solid fa-arrow-right"></i></a>
                 </div>
             </div>
