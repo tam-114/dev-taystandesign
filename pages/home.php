9283a24cb9698c97e1acaa00371f260bf6b611de
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-05 15:06:06 -0500

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -441,7 +441,7 @@ $checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" vie
                 </li>
             </ul>
             <ul class="cs-card-group">
-                <li class="cs-item py-lg-0 py-3" data-aos="fade-up" data-aos-duration="1500">
+                <li class="cs-item pb-lg-0 pb-5" data-aos="fade-up" data-aos-duration="1500">
                     <span class="cs-package">Starter Package</span>
                     <div class="cs-flex-wrapper flex-column justify-content-center align-items-center">
                         <span class="cs-price">$300</span>
@@ -498,7 +498,7 @@ $checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" vie
                     </div>
                     <a href="/contact" class="button-tertiary">Get started</a>
                 </li>
-                <li class="cs-item py-lg-0 py-3" data-aos="fade-up" data-aos-duration="1500">
+                <li class="cs-item pt-lg-0 pt-5" data-aos="fade-up" data-aos-duration="1500">
                     <span class="cs-package">Custom Package</span>
                     <div class="cs-flex-wrapper flex-column justify-content-center align-items-center">
                         <span class="cs-price">$900</span>
