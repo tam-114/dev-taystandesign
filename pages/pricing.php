58289f1f35ba1012f34641590a2b9077f58a52ca
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-04 21:17:40 -0500

    Update pricing.php

--- pages/pricing.php
+++ pages/pricing.php
@@ -1,107 +1,19 @@
-
-<!-- Pricing -->
-<section class="section-padding" id="pricing">
-    <div class="container pricing">
-        <div class="row row-cols-lg-2 row-cols-1 flex-lg-row flex-column pb-lg-5 pt-5" data-aos="fade-up" data-aos-duration="1500">
-            <div class="col">
-            <h4 class="subtitle-text">Pricing Plan</h4>
-                <h1>A <span class="highlight">Price</span> for Every Plan Out There</h1>
-            </div>
-            <div class="col d-flex justify-content-center align-items-center">
-                <p class="p-text">Check out these pricing plans to find the one that works best for you. Each requires a 30% deposit towards the final bill, and any plan can be modified to your needs!</p>
-            </div>
-        </div>
-<!-- 
-        <div class="row row-cols-lg-3 row-cols-1 flex-column flex-lg-row">
-            <div class="col align-self-center">
-                <div class="price-card d-flex justify-content-center align-items-center flex-column">
-                    <div class="text-lg-start align-self-lg-start ps-5">
-                        <h1>$500</h1>
-                        <h2>Starter Package</h2>
-                        <p>Best for 1-3 pages</p>
-                        <a href="< ?php echo BASE_URL; ?>contact" class="button-primary">Get Started</a>
-                    </div>
-
-                    <ul class="packages">
-                        <li><strong>Custom</strong> Domain</li>
-                        <li><strong>CMS</strong> Integration</li>
-                        <li><strong>FREE</strong> SSL Security</li>
-                        <li><strong>Professional</strong> Email</li>
-                        <li><strong>Explore</strong> Maintenance Plans</li>
-                        <li><strong>Recommended</strong> Hosting</li>
-                        <li><strong>3</strong> Revisions</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col align-self-center">
-                <div class="price_recommend">
-                    <div class="d-flex justify-content-end pe-4">
-                        <span class="popular-tag" data-aos="fade-up" data-aos-duration="1500">Popular</span>
-                    </div>
-
-                    <div class="d-flex justify-content-center align-items-center flex-column">
-                        <div class="text-lg-start align-self-lg-start ps-5">
-                            <h1>$900</h1>
-                            <h2>Pro Package</h2>
-                            <p>Best for 4-6 pages</p>
-                            <a href="< ?php echo BASE_URL; ?>contact" class="button-tertiary">Get started</a>
-                        </div>
-
-                        <ul class="packages">
-                            <li><strong>Custom</strong> Domain</li>
-                            <li><strong>CMS</strong> Integration</li>
-                            <li><strong>FREE</strong> SSL Security</li>
-                            <li><strong>Professional</strong> Email</li>
-                            <li><strong>Explore</strong> Maintenance Plans</li>
-                            <li><strong>Included</strong> Hosting</li>
-                            <li><strong>5</strong> Revisions</li>
-                            <li><strong>Catered</strong> Training Guide</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col align-self-center">
-                <div class="price-card d-flex justify-content-center align-items-center flex-column">
-                    <div class="text-lg-start align-self-lg-start ps-5">
-                        <h1>$1500</h1>
-                        <h2>Custom Package</h2>
-                        <p>Tailored to Your Needs</p>
-                        <a href="< ?php echo BASE_URL; ?>contact" class="button-primary">Get started</a>
-                    </div>
-
-                    <ul class="packages">
-                        <li><strong>Custom</strong> Domain</li>
-                        <li><strong>CMS</strong> Integration</li>
-                        <li><strong>FREE</strong> SSL Security</li>
-                        <li><strong>Professional</strong> Email</li>
-                        <li><strong>Included</strong> Maintenance Plan</li>
-                        <li><strong>Included</strong> Hosting</li>
-                        <li><strong>'#'</strong> Revisions</li>
-                        <li><strong>Data-driven</strong> SEO/Analytics</li>
-                        <li><strong>Catered</strong> Training Guide</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
-<!-- ============================================ -->
-<!--                   Pricing                    -->
-<!-- ============================================ -->
-
-<!-- <section id="pricing"> -->
-    <!-- <div class="cs-container">
-        <div class="cs-content">
-            <div class="cs-flex">
-                <span class="cs-topper">Pricing</span>
-                <h2 class="cs-title">Choose Pricing Plans</h2>
-            </div>
-            <a href="" class="cs-button-solid">Book Appointment</a>
-        </div> -->
-
 <?php
 $checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none"><path d="M19.2099 7.21001C19.117 7.11628 19.0064 7.04189 18.8845 6.99112C18.7627 6.94035 18.632 6.91422 18.4999 6.91422C18.3679 6.91422 18.2372 6.94035 18.1154 6.99112C17.9935 7.04189 17.8829 7.11628 17.79 7.21001L10.34 14.67L7.20995 11.53C7.11343 11.4368 6.99949 11.3635 6.87463 11.3143C6.74978 11.2651 6.61645 11.2409 6.48227 11.2432C6.34809 11.2456 6.21568 11.2743 6.0926 11.3278C5.96953 11.3813 5.85819 11.4585 5.76495 11.555C5.67171 11.6515 5.5984 11.7655 5.54919 11.8903C5.49999 12.0152 5.47586 12.1485 5.47818 12.2827C5.4805 12.4169 5.50923 12.5493 5.56272 12.6724C5.61622 12.7954 5.69343 12.9068 5.78995 13L9.62995 16.84C9.72291 16.9337 9.83351 17.0081 9.95537 17.0589C10.0772 17.1097 10.2079 17.1358 10.34 17.1358C10.472 17.1358 10.6027 17.1097 10.7245 17.0589C10.8464 17.0081 10.957 16.9337 11.05 16.84L19.2099 8.68001C19.3115 8.58637 19.3925 8.47272 19.4479 8.34622C19.5033 8.21972 19.5319 8.08312 19.5319 7.94501C19.5319 7.80691 19.5033 7.67031 19.4479 7.54381C19.3925 7.41731 19.3115 7.30366 19.2099 7.21001V7.21001Z" fill="#D90429"/></svg>';
 ?>
+<!-- Pricing -->
+<section class="section-padding" id="pricing">
+    <div class="container pricing">
+        <div class="d-flex align-items-center" data-aos="fade-up" data-aos-duration="1500">
+            <div class="">
+                <h4 class="subtitle-text">Pricing Plan</h4>
+                <h1>A <span class="highlight">Price</span> for Every Plan Out There</h1>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <p class="p-text">Check out these pricing plans to find the one that works best for you. Each requires a 30% deposit towards the final bill, and any plan can be modified to your needs!</p>
+            </div>
+        </div>
+
         <div class="cs-wrapper">
             <ul class="cs-ul" data-aos="fade-up" data-aos-duration="1500">
             <span class="p-text fw-bold">Every plan will have:</span>
@@ -150,7 +62,6 @@ $checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" vie
                     <a href="/contact" class="button-primary">Get started</a>
                 </li>
                 <li class="cs-item cs-popular" data-aos="fade-up" data-aos-duration="1800">
-                <!-- <span class="popular-tag" data-aos="fade-up" data-aos-duration="1800">Popular</span> -->
                     <span class="cs-package">Pro Package</span>
                     <div class="cs-flex-wrapper flex-column justify-content-center align-items-center">
                         <span class="cs-price">$600</span>
