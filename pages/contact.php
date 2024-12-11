63908e22a99c466090ca6bcc1d03c5552791b985
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-11 08:57:08 -0500

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -50,7 +50,7 @@ $color = "#1a1a1a";
                 Message
                 <textarea class="cs-input cs-textarea" required name="message" id="message" placeholder="Write message..."></textarea>
             </label>
-            <div class="">
+            <div class="d-flex justify-content-center align-items-start flex-row">
                 <input type="checkbox" name="privacy" id="privacy">
                 <label class="cs-label d-block">
                     I agree to have my information collected for contacting purposes in accordance with <a href="/privacy">Privacy Policy</a> and <a href="/terms">Terms and Conditions</a>.
