570f33b2cebb1364301ba617b8b1ecb9bc6b7c32
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-13 11:58:43 -0500

    Update header.php

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 <!-- Navbar --->
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="./assets/images/taystan-logo2.png" alt="" class="logo" ></a>
+    <a class="navbar-brand" href="#"><img src="/assets/images/taystan-logo2.png" alt="" class="logo" ></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
