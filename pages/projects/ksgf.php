cfa78bf7f8c6fc7d72b6738807d0e089d8f32955
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-13 11:55:36 -0500

    ksgf page

--- pages/projects/ksgf.php
+++ pages/projects/ksgf.php
@@ -1,3 +1,15 @@
 <main>
-
+<div class="container">
+    <div class="row flex-column flex-lg-row">
+        <div class="col-lg-6">
+            <img src="/images/ksgf_old.png" alt="">
+            <img src="/images/ksgf_new.png" alt="">
+        </div>
+        <div class="col-lg-6">
+            <div class="position-sticky">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi non repellendus neque amet quam reiciendis nobis odit veritatis optio est dolorum sint magnam, consectetur repellat praesentium soluta molestiae molestias. Provident!</p>
+            </div>
+        </div>
+    </div>
+</div>
 </main>
