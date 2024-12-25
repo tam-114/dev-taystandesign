b7786b22cc89508fd14e88583c1e84de2db1823f
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 12:23:23 -0500

    Update templates.php

--- pages/templates.php
+++ pages/templates.php
@@ -1,21 +1,35 @@
-<div class="container">
-    <div class="row row-cols-lg-2 row-cols-1 flex-column flex-lg-row">
-        <div class="col">
-            <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
-            <div class="py-3">
-                <h2>Dentistry Template</h2>
-                <p>This is a template landing page for a Dentistry website. The placeholder business information is for the Orlando Dentist Center.</p>
-                <a href="#" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website">Go to Dentistry Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+<section class="section-padding">
+    <div class="container pb-0">
+        <div class="d-flex align-items-center flex-column pb-3" data-aos="fade-up" data-aos-duration="1500">
+            <div class="text-center">
+                <h4 class="subtitle-text">Templates</h4>
+                <h1 class="h1-text">Templates <span class="highlight">Tailored</span> for Success</h1>
             </div>
-        </div>
-
-        <div class="col">
-            <img src="./assets/images/Salon_Mockup.png" alt="" class="img-fluid templates-img">
-            <div class="py-3">
-                <h2>Hair Salon Template</h2>
-                <p>This is a template landing page for a Hair Salon website. The placeholder business information is for the Beauty Royale Hair Salon.</p>
-                <a href="https://tam-114.github.io/beautyroyale" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website" target="_blank">Go to Hair Salon Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+            <div class="d-flex justify-content-center align-items-center">
+                <p class="p-text text-center">Discover a selection of ready-made websites crafted with purpose and precision. From vibrant designs to seamless functionality, these templates are built to elevate brands and engage audiences effortlessly.</p>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="container">
+        <div class="row row-cols-lg-2 row-cols-1 flex-column flex-lg-row">
+            <div class="col">
+                <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
+                <div class="py-3">
+                    <h2>Dentistry Template</h2>
+                    <p>This is a template landing page for a Dentistry website. The placeholder business information is for the Orlando Dentist Center.</p>
+                    <a href="#" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website">Go to Dentistry Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+
+            <div class="col">
+                <img src="./assets/images/Salon_Mockup.png" alt="" class="img-fluid templates-img">
+                <div class="py-3">
+                    <h2>Hair Salon Template</h2>
+                    <p>This is a template landing page for a Hair Salon website. The placeholder business information is for the Beauty Royale Hair Salon.</p>
+                    <a href="https://tam-114.github.io/beautyroyale" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website" target="_blank">Go to Hair Salon Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
