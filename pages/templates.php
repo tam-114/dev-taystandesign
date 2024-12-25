222f6bad474b32e672accdb3ef13b7ea7fc40b6f
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 12:06:22 -0500

    beauty royale

--- pages/templates.php
+++ pages/templates.php
@@ -1,11 +1,20 @@
 <div class="container">
-    <div class="row row-cols-lg-2 row-cols-1">
+    <div class="row row-cols-lg-2 row-cols-1 flex-column flex-lg-row">
         <div class="col">
             <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
             <div class="py-3">
                 <h2>Dentistry Template</h2>
                 <p>This is a template landing page for a Dentistry website. The placeholder business information is for the Orlando Dentist Center.</p>
                 <a href="#" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website">Go to Dentistry Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+            </div>
+        </div>
+
+        <div class="col">
+            <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
+            <div class="py-3">
+                <h2>Hair Salon Template</h2>
+                <p>This is a template landing page for a Hair Salon website. The placeholder business information is for the Beauty Royale Hair Salon.</p>
+                <a href="https://tam-114.github.io/beauty-royale" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website">Go to Hair Salon Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
             </div>
         </div>
     </div>
