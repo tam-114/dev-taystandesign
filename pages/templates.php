4868bdcc5e5f9d2126f49c613e03010b0154dcc8
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 11:54:38 -0500

    Update templates.php

--- pages/templates.php
+++ pages/templates.php
@@ -2,7 +2,7 @@
     <div class="row row-cols-lg-2 row-cols-1">
         <div class="col">
             <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
-            <div class="py-2">
+            <div class="py-3">
                 <h2>Dentistry</h2>
                 <p>This is a template landing page for a Dentistry website. The placeholder business information is for the Orlando Dentist Center.</p>
             </div>
