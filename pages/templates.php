dee950f3020239e3ab1cd9d305afc008777eb4b9
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 12:18:35 -0500

    Update templates.php

--- pages/templates.php
+++ pages/templates.php
@@ -14,7 +14,7 @@
             <div class="py-3">
                 <h2>Hair Salon Template</h2>
                 <p>This is a template landing page for a Hair Salon website. The placeholder business information is for the Beauty Royale Hair Salon.</p>
-                <a href="https://tam-114.github.io/beauty-royale" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website">Go to Hair Salon Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+                <a href="https://tam-114.github.io/beauty-royale" class="btn-secondary ps-0" aria-label="Link to Dentistry Template Website" target="_blank">Go to Hair Salon Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
             </div>
         </div>
     </div>
