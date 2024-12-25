ddb51df16aa923532240e67d52d02b1359e8049e
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 12:30:23 -0500

    Update templates.php

--- pages/templates.php
+++ pages/templates.php
@@ -12,7 +12,7 @@
     </div>
 
     <div class="container">
-        <div class="row row-cols-lg-2 row-cols-1 flex-column flex-lg-row">
+        <div class="row row-cols-lg-2 row-cols-1 flex-column flex-lg-row gy-3">
             <div class="col">
                 <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
                 <div class="py-3">
