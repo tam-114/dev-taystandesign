cd608aac10fa795ba164683c98c4be0b8d0f344d
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 11:57:19 -0500

    Update templates.php

--- pages/templates.php
+++ pages/templates.php
@@ -3,7 +3,7 @@
         <div class="col">
             <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid templates-img">
             <div class="py-3">
-                <h2>Dentistry</h2>
+                <h2>Dentistry Template</h2>
                 <p>This is a template landing page for a Dentistry website. The placeholder business information is for the Orlando Dentist Center.</p>
                 <a href="#" class="btn-secondary ps-0" aria-label="Link to Dentistry website">Go to Dentistry Website <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
             </div>
