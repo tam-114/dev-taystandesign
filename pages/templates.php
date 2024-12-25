0306a7d2584dba81903dc6e33bc41b50d21c562d
Taystan <52808097+tam-114@users.noreply.github.com>
2024-12-25 11:49:45 -0500

    Update templates.php

--- pages/templates.php
+++ pages/templates.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row row-cols-lg-2 row-cols-1">
         <div class="col">
-            <img src="./assets/images/Dentist_Mockup.png" alt="" class="img-fluid">
+            <img src="./assets/images/Dentistry_Mockup.png" alt="" class="img-fluid">
         </div>
     </div>
 </div>
