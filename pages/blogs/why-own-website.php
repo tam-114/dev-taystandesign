7bebdc98c6ece9e004c4fa6fdaaba9e98a8c49a8
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-20 22:15:32 -0500

    date

--- pages/blogs/why-own-website.php
+++ pages/blogs/why-own-website.php
@@ -1,5 +1,9 @@
 <section>
     <div class="container mx-auto d-flex justify-content-center align-items-center blog-container">
+        <!-- Cover Image -->
         <img src="/assets/images/Own_Website.png" alt="" class="img-fluid">
+
+        <!-- Content -->
+         <span>Monday, January 20, 2025</span>
     </div>
 </section>
