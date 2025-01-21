d6819ee9387adc7baf6a698485f6882d355ee4af
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-20 22:16:04 -0500

    Update why-own-website.php

--- pages/blogs/why-own-website.php
+++ pages/blogs/why-own-website.php
@@ -1,9 +1,9 @@
 <section>
-    <div class="container mx-auto d-flex justify-content-center align-items-center blog-container">
+    <div class="container mx-auto d-flex justify-content-center align-items-center blog-container flex-column">
         <!-- Cover Image -->
         <img src="/assets/images/Own_Website.png" alt="" class="img-fluid">
 
         <!-- Content -->
-         <span>Monday, January 20, 2025</span>
+         <span class="p-2">Monday, January 20, 2025</span>
     </div>
 </section>
