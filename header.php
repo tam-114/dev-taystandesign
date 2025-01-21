25c0a2ba35f3e67a207054bd2c053b4f695b5da6
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-21 08:30:40 -0500

    Update header.php

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
     <link rel="canonical" href="https://taystan.design<?php echo parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); ?>" />
     <link rel="icon" href="/assets/images/taystan-logo2.png" type="image/x-icon">
     <base href="https://taystan.design/">
-    <title>Taystan Design</title>
+    <title>Taystan Design - Freelance Web Designer & Developer</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
