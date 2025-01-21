142eced4a9834e3e277b4b600469834ee2ce6c4d
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-20 22:58:15 -0500

    rename blogs to blog

--- pages/blog/index.php
+++ pages/blog/index.php
@@ -2,11 +2,11 @@
     <div class="container pb-0">
         <div class="d-flex align-items-center flex-column pb-3" data-aos="fade-up" data-aos-duration="1500">
                 <div class="text-center">
-                    <h4 class="subtitle-text">Blogs</h4>
+                    <h4 class="subtitle-text">Blog</h4>
                     <h1 class="h1-text">All <span class="highlight">Posts</span> In One Place</h1>
                 </div>
                 <div class="d-flex justify-content-center align-items-center">
-                    <p class="p-text text-center">Welcome to my blog! Here you'll find insights, tips, and practical advice for all things web development & design.</p>
+                    <p class="p-text text-center">Here you'll find articles with insights, tips, and practical advice for all things web development & design.</p>
                 </div>
         </div>
     </div>
