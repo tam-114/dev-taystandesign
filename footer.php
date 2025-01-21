20296bdf4cf9ed9901cc52795f1b049d927bd965
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-20 23:14:32 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -49,7 +49,7 @@
           <div class="col-lg-3 col-12 pb-5 pb-lg-0">
             <h3 class="footer_h3">Working Hours</h3>
             <p class="footer_p">Mon-Fri: 9AM - 2PM</p>
-            <p class="footer_p">Saturday: 12AM - 5PM</p>
+            <p class="footer_p">Saturday: 12PM - 5PM</p>
             <p class="footer_p">Sunday: OFF</p>
           </div>
         </div>
