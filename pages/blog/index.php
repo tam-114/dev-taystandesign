486f2d352bffb6ab8e7695236ef3d716dd85b189
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-20 23:05:25 -0500

    Update index.php

--- pages/blog/index.php
+++ pages/blog/index.php
@@ -1,13 +1,22 @@
 <section class="section-padding">
     <div class="container pb-0">
         <div class="d-flex align-items-center flex-column pb-3" data-aos="fade-up" data-aos-duration="1500">
-                <div class="text-center">
-                    <h4 class="subtitle-text">Blog</h4>
-                    <h1 class="h1-text">All <span class="highlight">Posts</span> In One Place</h1>
+            <div class="text-center">
+                <h4 class="subtitle-text">Blog</h4>
+                <h1 class="h1-text">All <span class="highlight">Posts</span> In One Place</h1>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <p class="p-text text-center">Here you'll find articles with insights, tips, and practical advice for all things web development & design.</p>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="/assets/images/Own_Website.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Why Should You Own a Website?</h5>
+                    <p class="card-text">Discover why owning a website gives you control, credibility, and a reliable online presence, ensuring your business thrives even when social media platforms change or disappear.</p>
+                    <a href="/blog/why-own-website" class="card-link">Read more</a>
                 </div>
-                <div class="d-flex justify-content-center align-items-center">
-                    <p class="p-text text-center">Here you'll find articles with insights, tips, and practical advice for all things web development & design.</p>
-                </div>
+            </div>
         </div>
     </div>
 </section>
