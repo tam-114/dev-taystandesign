d06cfbfca2b7322b432f15fd03a9a9de6eee3bc0
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-21 08:37:50 -0500

    Update why-own-website.php

--- pages/blog/why-own-website.php
+++ pages/blog/why-own-website.php
@@ -4,7 +4,7 @@
         <img src="/assets/images/Own_Website.png" alt="" class="img-fluid blog-img">
 
         <!-- Content -->
-         <article class="container mt-2">
+         <article class="container mt-3">
             <span class="blog-date">Monday, January 20, 2025</span>
             <h2 class="blog-title">Why Should You Own a Website?</h2>
             <p>Have you been thinking about what you'd do since the talk of TikTok being banned? What if it wasn't just TikTok, maybe Instagram or another social media platform suddenly disappeared? Or if your account got banned for no clear reason? Unfortunately, this isn't just a “what if” scenario. Platforms like TikTok may have only been banned for a whooping 12 hours, but and businesses that rely solely on social media often find themselves in a tough spot when things change overnight. That's why owning your own website can be the best move going forward. Here's why having a website can make all the difference.</p>
