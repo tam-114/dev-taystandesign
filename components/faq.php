7b3b537d2ae7a9b9225ae466fc3f8362d39c13a2
Taystan <52808097+tam-114@users.noreply.github.com>
2025-01-24 10:13:52 -0500

    component

---
+++ components/faq.php
@@ -0,0 +1,54 @@
+<div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            <p class="p-text fw-500 mb-0"> What is your experience in web design/development?</p>
+        </button>
+        </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">I have over 3+ years of professional experience as a full-time Web Designer/Developer. I'm a graduate from the University of Central Florida, with my Bachelor's in Digital Media: Web Design and minor in Information Technology.</div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+            <p class="p-text fw-500 mb-0">How long does it typically take to complete a project?</p>
+        </button>
+        </h2>
+        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">The length of time typically depends on the project’s needs, say from a website re-design to starting from scratch, but it can range to be between 2-6 weeks for delivery.</div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+            <p class="p-text fw-500 mb-0">How do you handle revisions or change requests during the project?</p>
+        </button>
+        </h2>
+        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">The amount of revisions are included with the purchased packages, and is essentially inline with each milestone. Depending on the size of the Revision/Change Request there may be an update to the timeline of completion, but I will always do my best to meet the deadline. 
+            <br><br>
+            <em>*A Change Request would refer to adding and/or changing parts of the site content entirely outside of the scope, while a Revision would be modifying existing content.</em></div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingFour">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+            <p class="p-text fw-500 mb-0">Do you offer ongoing support and maintenance?</p>
+        </button>
+        </h2>
+        <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Yes! There is an upfront cost for yearly maintenance and hosting of the site, or priced per month depending on the package selected. Depending on the request, changes can be included in the maintenance fee or an additional $30/hr until completion.</div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingFive">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+            <p class="p-text fw-500 mb-0">Do you require a deposit?</p>
+        </button>
+        </h2>
+        <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Yes, a minimum of 30% upfront, then installments after milestones and revisions are completed. Once you're satisfied with the result, an invoice will be sent with a grace period of 15 days for payment to be due.</div>
+        </div>
+    </div>
+</div>
