0cad682880ee82a6ffc8d6a48a02412ddef9a880
Taystan <52808097+tam-114@users.noreply.github.com>
2025-04-04 11:58:11 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -30,7 +30,7 @@
             <br>
             <div class="footer_contact-inline">
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
-              <a href="mailto:info@taystan.design" class="footer_contact">info@taystan.design</a>
+              <a href="mailto:taylor.stanley@taystan.design" class="footer_contact">taylor.stanley@taystan.design</a>
             </div>
             <br>
             <br>
